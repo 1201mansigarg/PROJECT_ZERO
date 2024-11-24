73bd1322debe48ff4ddc20d62c2618a089baad0a
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2024-11-24 19:40:00 +0530

    Admin authentication added

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
+<<<<<<< HEAD
     const token = req.headers["authorization"]?.split(" ")[1];
+=======
+    const token = req.headers["authorization"]?.split(" ")[1]; // Get the token from Authorization header
+>>>>>>> 549c5bf (Admin authentication added)
 
     if (!token) {
       return res.status(401).json({ message: "No token provided" });
@@ -11,7 +15,11 @@ export const verifyToken = (req, res, next) => {
       if (err) {
         return res.status(403).json({ message: "Invalid or expired token" });
       }
+<<<<<<< HEAD
       req.user = decoded;
+=======
+      req.user = decoded; // Attach user info to request object
+>>>>>>> 549c5bf (Admin authentication added)
       next();
     });
   };
